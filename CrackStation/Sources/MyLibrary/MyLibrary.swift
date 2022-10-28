3de14e4c6e380b3f6dbcb46fab9aad2e1cc4b573
BenChou <choupa@oregonstate.edu>
2022-10-28 05:00:58 -0700

    add Decrypt protocol & POCv2 resolve

--- CrackStation/Sources/MyLibrary/MyLibrary.swift
+++ CrackStation/Sources/MyLibrary/MyLibrary.swift
@@ -1,8 +1,9 @@
 import Foundation
 import CryptoKit
 
-@available(macOS 10.15, *)
-public class MyLibrary {
+//@available(macOS 10.15, *)
+public class MyLibrary: Decrypter {
+    required public init() { }
     static func loadDictionaryFromDisk() throws -> [String : String] {
         guard let path = Bundle.module.url(forResource: "sha1", withExtension: "json") else { return [:] }
         
@@ -15,28 +16,28 @@ public class MyLibrary {
             return [:]
         }
     }
-    
+    /*
     static func encryptUsingSha1(from input: String) -> String {
         let inputData = Data(input.utf8)
         let output = Insecure.SHA1.hash(data: inputData)
         return output.description
     }
-    
+    */
     /// Either returns the cracked plain-text password
     /// or, if unable to crack, then returns nil.
-    public func crack(password: String) -> String? {
+    public func decrypt(shaHash: String) -> String? {
         let lookuptable = try! MyLibrary.loadDictionaryFromDisk()
         var crackPass = ""
         var letter = ""
         
-        if password.count % 8 != 0 {
+        if shaHash.count % 8 != 0 {
             //print("1")
             return nil
             
         } else {
             //print("password count = ", password.count)
-            for index in stride(from: 0, to: password.count, by: 1) {
-                letter += String(password[index])
+            for index in stride(from: 0, to: shaHash.count, by: 1) {
+                letter += String(shaHash[index])
                 //print("index = ", index)
                 //print("letter = ", letter)
                 
